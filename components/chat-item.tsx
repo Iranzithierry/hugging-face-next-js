312047ca4256058ebb230d7e3955757ac695469e
Iranzi Dev <iranzithierry12@gmail.com>
2024-01-02 20:29:06 +0200

    Update chat-item.tsx

--- components/chat-item.tsx
+++ components/chat-item.tsx
@@ -9,8 +9,8 @@ export interface chatItemProps {
 export default function ChatItem({ message, role }: chatItemProps) {
   return (
     <div className={`w-full px-2 sm:px-4 md:px-6 lg:px-7 xl:px-8  py-4 overflow-hidden ${role === 'user' ? 'bg-white ' : 'bg-gray-100'}`}>
-      <div className='max-w-screen-md mx-auto grid grid-cols-12'>
-        <div className='col-span-1 w-fit py-3'>
+      <div className='max-w-screen-md mx-auto flex flex-row space-x-3'>
+        <div className='w-fit py-3'>
           <div className={`h-8 w-8 flex  items-center justify-center ${role == 'user' ? 'bg-green-500 text-white' : 'bg-gray-900 text-white'} rounded-md px-2 py-1`}>
             {role === 'user' ?
               <UserIcon /> :
@@ -18,7 +18,7 @@ export default function ChatItem({ message, role }: chatItemProps) {
             }
           </div>
         </div>
-        <div className='col-span-11'>
+        <div>
           <p className='font-sans font-base'>{message}</p>
         </div>
       </div>
